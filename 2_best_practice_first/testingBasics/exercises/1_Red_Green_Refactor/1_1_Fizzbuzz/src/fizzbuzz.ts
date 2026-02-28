f02577cfee5546b3af20f2a8e666dcb467564f9c
Manuel Desole <desole.manuel@gmail.com>
2026-02-28 13:05:51 +0100

    [green]: returns "Fizz" for 6

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export const fizzbuzz = (value: number) => {
     if (value === 15) return "FizzBuzz"
-    if (value === 3) return "Fizz"
+    if (value % 3 === 0) return "Fizz"
     if (value === 5) return "Buzz"
     return "1"
 }
