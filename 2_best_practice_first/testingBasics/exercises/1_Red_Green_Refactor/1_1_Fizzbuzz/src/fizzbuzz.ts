2c2e85e027593d12e127e5752ad20a9faefe7397
Manuel Desole <desole.manuel@gmail.com>
2026-02-28 12:52:21 +0100

    [red]: returns "1" for 1

---
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export const fizzbuzz = (value: number) => {
+
+}
--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+    it('returns "1" for 1', () => {
+        expect(fizzbuzz(1)).toBe("1")
+    })
 });
