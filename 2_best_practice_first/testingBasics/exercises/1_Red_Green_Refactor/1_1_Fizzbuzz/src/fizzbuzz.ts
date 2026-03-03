086841eced135998d28ecf31ef10007daf1476f0
Manuel Desole <desole.manuel@gmail.com>
2026-03-03 19:21:17 +0100

    [green]: returns "the number should be among 1 and 100"  for 102

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export const fizzbuzz = (value: number) => {
-    if (value < 1) throw new Error("the number should be among 1 and 100")
+    if (value < 1 || value > 100) throw new Error("the number should be among 1 and 100")
     const isMultipleOfThree = value % 3 === 0
     const isMultipleOfFive = value % 5 === 0
 
