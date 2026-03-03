06f5977b1310b0cfe8ee01c2bb88e03df3c7afaa
Manuel Desole <desole.manuel@gmail.com>
2026-03-03 19:50:44 +0100

    [green]: returns true for "mom"

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { palindromeChecker } from "."
+
+
 
 describe('palindrome checker', () => {
-
+    it('returns true for "mom"', () => {
+        expect(palindromeChecker("mom")).toBeTruthy()
+    })
 })
---
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -0,0 +1,3 @@
+export const palindromeChecker = (value: string) => {
+    return true
+}
