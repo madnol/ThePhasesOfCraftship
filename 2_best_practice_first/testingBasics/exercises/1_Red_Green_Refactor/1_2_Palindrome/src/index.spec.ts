962e70ad1f9d56988c4b547c7b8a1563e1d4b622
Manuel Desole <desole.manuel@gmail.com>
2026-03-03 20:15:04 +0100

    [refactor]: use it.each for tests with truthy result

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -3,23 +3,12 @@ import { palindromeChecker } from "."
 
 
 describe('palindrome checker', () => {
-    it('returns true for "mom"', () => {
-        expect(palindromeChecker("mom")).toBeTruthy()
-    })
 
-    it('returns true for "Mom"', () => {
-        expect(palindromeChecker("Mom")).toBeTruthy()
-    })
-
-    it('returns true for "MoM"', () => {
-        expect(palindromeChecker("MoM")).toBeTruthy()
+    it.each(["mom", "Mom", "MoM", "xMomx"])('returns true for "%s"', (value) => {
+        expect(palindromeChecker(value)).toBeTruthy()
     })
 
     it('returns false for "Momx"', () => {
         expect(palindromeChecker("Momx")).toBeFalsy()
-    })
-
-    it('returns true for "xMomx"', () => {
-        expect(palindromeChecker("xMomx")).toBeTruthy()
     })
 })
