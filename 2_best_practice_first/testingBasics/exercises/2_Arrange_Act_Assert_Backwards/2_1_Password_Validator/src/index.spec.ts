adc7d8e5eec683b340bdf33ac5fa4315d7dc0608
Manuel Desole <desole.manuel@gmail.com>
2026-03-06 01:59:06 +0100

    [refactor]: group one digit tests

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -5,6 +5,12 @@ const INVALID_LENGTH = {
   message: "Must be between 5 and 15 characters long"
 }
 
+
+const NO_DIGIT_INCLUDED = {
+  type: "NoDigitIncluded",
+  message: "Must have at least one digit"
+}
+
 describe('password validator', () => {
 
   describe('checking between 5 to 15 characters long', () => {
@@ -25,29 +31,23 @@ describe('password validator', () => {
       })
   })
 
-  it('knows "Notapassword1" contains at least one digit', () => {
-    let output = PasswordValidator.validate("Notapassword1")
+  describe('checking for at least one digit', () => {
+    it.each([
+      ["Notapassword1", true, []],
+      ["Zetman0", true, []],
+      ["maxwellTheBe", false, [NO_DIGIT_INCLUDED]]
+    ])
+      ('knows %s should return %s', (input, result, errors) => {
+        let output = PasswordValidator.validate(input)
 
-    expect(output.result).toBeTruthy()
-    expect(output.errors).toHaveLength(0)
+        expect(output.result).toBe(result)
+        expect(output.errors).toHaveLength(errors.length)
+        if (errors.length) {
+          expect(output.errors[0].type).toEqual(errors[0].type)
+          expect(output.errors[0].message).toEqual(errors[0].message)
+        }
+      })
   })
-
-  it('knows "Zetman0" contains at least one digit', () => {
-    let output = PasswordValidator.validate("Notapassword1")
-
-    expect(output.result).toBeTruthy()
-    expect(output.errors).toHaveLength(0)
-  })
-
-  it('knows "maxwellTheBe" does not contains any digit', () => {
-    let output = PasswordValidator.validate("maxwellTheBe")
-
-    expect(output.result).toBeFalsy()
-    expect(output.errors).toHaveLength(1)
-    expect(output.errors[0].type).toEqual("NoDigitIncluded")
-    expect(output.errors[0].message).toEqual('Must have at least one digit')
-  })
-
 
 })
 
