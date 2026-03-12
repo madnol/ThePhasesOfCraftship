e3dbf2f1ea4d8638beff3abe4bec3f03f9dd9e31
Manuel Desole <desole.manuel@gmail.com>
2026-03-12 17:50:03 +0100

    [green]: return {min: 7, max: 7, elements: 1, average: 7.0} for the sequence [7]

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { statsCalculator } from "."
 
 describe('stats calculator', () => {
 
-
+    it('return {min: 7, max: 7, elements: 1, average: 7.0} for the sequence [7]', () => {
+        expect(statsCalculator([7])).toEqual({ min: 7, max: 7, elements: 1, average: 7.0 })
+    })
 })
---
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,3 @@
+export const statsCalculator = (sequence: number[]) => {
+    return { min: 7, max: 7, elements: 1, average: 7.0 }
+} 
