ccc1c50964735b10d3e669a0e10e217f3618a561
Manuel Desole <desole.manuel@gmail.com>
2026-03-12 23:05:25 +0100

    [green]: for the sequence [ 2, 4, 21, -8, 53, 40 ] returns { min: -8, max: 53, elements: 6, average: 18.666666666666668 }

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -7,7 +7,8 @@ describe('stats calculator', () => {
         [[3], { min: 3, max: 3, elements: 1, average: 3.0 }],
         [[3, 3], { min: 3, max: 3, elements: 2, average: 3.0 }],
         [[3, 9], { min: 3, max: 9, elements: 2, average: 6.0 }],
-        [[-10, -3, -7], { min: -10, max: -3, elements: 3, average: -6.666666666666667 }]
+        [[-10, -3, -7], { min: -10, max: -3, elements: 3, average: -6.666666666666667 }],
+        [[2, 4, 21, -8, 53, 40], { min: -8, max: 53, elements: 6, average: 18.666666666666668 }],
     ])('for the sequence %s returns %s', (sequence, output) => {
         expect(statsCalculator(sequence)).toEqual(output)
     })
