04e4467d794e29e4cc5b7671d97218b65af0b9ce
Manuel Desole <desole.manuel@gmail.com>
2026-03-12 23:12:45 +0100

    [green]: add [0] and [-5, 5, -3, 3] to triangulate zero and average zero edge cases

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -5,8 +5,10 @@ describe('stats calculator', () => {
     it.each([
         [[7], { min: 7, max: 7, elements: 1, average: 7.0 }],
         [[3], { min: 3, max: 3, elements: 1, average: 3.0 }],
+        [[0], { min: 0, max: 0, elements: 1, average: 0.0 }],
         [[3, 3], { min: 3, max: 3, elements: 2, average: 3.0 }],
         [[3, 9], { min: 3, max: 9, elements: 2, average: 6.0 }],
+        [[-5, 5, -3, 3], { min: -5, max: 5, elements: 4, average: 0.0 }],
         [[-10, -3, -7], { min: -10, max: -3, elements: 3, average: -6.666666666666667 }],
         [[2, 4, 21, -8, 53, 40], { min: -8, max: 53, elements: 6, average: 18.666666666666668 }],
     ])('for the sequence %s returns %s', (sequence, output) => {
