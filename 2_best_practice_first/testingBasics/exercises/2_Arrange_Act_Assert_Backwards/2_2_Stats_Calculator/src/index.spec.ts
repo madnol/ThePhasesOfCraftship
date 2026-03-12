20cc5f57897901c0875aa942f16dad8e7c7ea57b
Manuel Desole <desole.manuel@gmail.com>
2026-03-12 23:33:58 +0100

    [refactor]: improve readibility of it.each using object syntax

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -3,15 +3,15 @@ import { statsCalculator } from "."
 describe('stats calculator', () => {
 
     it.each([
-        [[7], { min: 7, max: 7, elements: 1, average: 7.0 }],
-        [[3], { min: 3, max: 3, elements: 1, average: 3.0 }],
-        [[0], { min: 0, max: 0, elements: 1, average: 0.0 }],
-        [[3, 3], { min: 3, max: 3, elements: 2, average: 3.0 }],
-        [[3, 9], { min: 3, max: 9, elements: 2, average: 6.0 }],
-        [[-5, 5, -3, 3], { min: -5, max: 5, elements: 4, average: 0.0 }],
-        [[-10, -3, -7], { min: -10, max: -3, elements: 3, average: -6.666666666666667 }],
-        [[2, 4, 21, -8, 53, 40], { min: -8, max: 53, elements: 6, average: 18.666666666666668 }],
-    ])('for the sequence %s returns %s', (sequence, output) => {
+        { sequence: [7], output: { min: 7, max: 7, elements: 1, average: 7.0 } },
+        { sequence: [3], output: { min: 3, max: 3, elements: 1, average: 3.0 } },
+        { sequence: [0], output: { min: 0, max: 0, elements: 1, average: 0.0 } },
+        { sequence: [3, 3], output: { min: 3, max: 3, elements: 2, average: 3.0 } },
+        { sequence: [3, 9], output: { min: 3, max: 9, elements: 2, average: 6.0 } },
+        { sequence: [-5, 5, -3, 3], output: { min: -5, max: 5, elements: 4, average: 0.0 } },
+        { sequence: [-10, -3, -7], output: { min: -10, max: -3, elements: 3, average: -6.666666666666667 } },
+        { sequence: [2, 4, 21, -8, 53, 40], output: { min: -8, max: 53, elements: 6, average: 18.666666666666668 } },
+    ])('for the sequence %sequence returns %output', ({ sequence, output }) => {
         expect(statsCalculator(sequence)).toEqual(output)
     })
 
